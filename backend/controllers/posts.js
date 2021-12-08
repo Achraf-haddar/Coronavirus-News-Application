210dbd8793f90c6182a29b3e57386f5a3235fbf8
Achraf-haddar <62722610+Achraf-haddar@users.noreply.github.com>
2021-12-08 19:45:47 +0200

    JSX Adding Components and Styles

--- backend/controllers/posts.js
+++ backend/controllers/posts.js
@@ -1,3 +1,28 @@
-export const getPosts = (req, res) => {
-    res.send("This works!")
+import PostMessage from "../models/postMessage.js";
+
+export const getPosts = async (req, res) => {
+    try{
+        const postMessages = await PostMessage.find(); // Async because it takes time to retrieve data from database
+        console.log(postMessages);
+        res.status(200).json(postMessages); // Everything is OK
+    } catch (error) {
+        res.status(404).json({message: error.message});
+    }
+}
+
+export const createPost = async(req, res) => {
+    const post = req.body;  //retreive info from the filled form   
+    // Create a new PostMessage object with the info retrieved
+    const newPost = new PostMessage(post);
+
+    try{
+        // Save in the db
+        await newPost.save();
+        // Successful creation
+        res.status(201).json(newPost);
+    } catch (error){
+        res.status(409).json({message: error.message});
+
+    }
+    res.send('Post Creation');
 }
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,37 @@
 import React from "react";
 
+import Posts from "./components/Posts/Posts";
+import Form from "./components/Form/Form";
+
+// Import materials components
+import { Container, AppBar, Typography, Grid } from '@material-ui/core';
+import useStyles from './styles';
+
+import Post1 from './images/Post1.PNG'
 const App = () => {
+    const classes = useStyles();
+
     return (
-        <div>
-            <h1>App</h1>
-        </div>
+        <Container maxWidth="lg">
+            <AppBar className={classes.appBar} position="static" color="inherit">
+                <Typography className={classes.heading} variant="h2" align="center">Posts</Typography>
+                <img className={classes.image} src={Post1} alt="Posts1" height="160"/>
+            </AppBar>
+            {/*<Grow in="true">  Provide Simple Animation */}
+                <Container>
+                    <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+                        <Grid item xs={12} sm={7}>
+                            <Posts />
+                        </Grid>
+                        <Grid item xs={12} sm={4}>
+                            <Form />
+                        </Grid>
+
+                    </Grid>
+                </Container>
+            {/*</Grow>*/}
+
+        </Container>
     );
 } 
 export default App;
