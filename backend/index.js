c01e3e4d0220de099c1929c6af855082a99ad383
Achraf-haddar <62722610+Achraf-haddar@users.noreply.github.com>
2021-12-08 02:03:03 +0200

    Structuring backend folder

--- backend/index.js
+++ backend/index.js
@@ -2,9 +2,14 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import postRoutes from './routes/posts.js';
 
 // Initialize the app
 const app = express();
+
+// Adding routes
+app.use('/posts', postRoutes);    // use /posts as parent path for all routes
+
 // maximum input size for the images
 app.use(bodyParser.json({limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
@@ -14,6 +19,8 @@ app.use(cors());
 const CONNECTION_URL = 'mongodb+srv://Achraf123:Achraf123@cluster0.lwc1u.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
+
 mongoose.connect(CONNECTION_URL)
-    .then(()=>{console.log(`Server running on Port: ${PORT}`)})
-    .catch((error) => console.log(error.message));
+    .then(() => app.listen(PORT, () => console.log(`Server running on Port: ${PORT}`))) //if connection is successful
+    .catch((error) => console.log(error.message)); //if connection is unsuccessful ==> log the error on console
+
