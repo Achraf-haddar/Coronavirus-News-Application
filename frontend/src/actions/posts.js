dca86fea00209cc8a9aa8ea5ec8a2bf6e3cccdc1
Achraf-haddar <62722610+Achraf-haddar@users.noreply.github.com>
2021-12-10 19:29:26 +0200

    Updates

--- frontend/src/actions/posts.js
+++ frontend/src/actions/posts.js
@@ -1,3 +1,4 @@
+import { FETCH_ALL, DELETE, CREATE, UPDATE } from '../constants/actionTypes';
 import * as api from '../api';
 
 // Action Creators: Functions that return actions
@@ -7,7 +8,7 @@ export const getPosts = () => async (dispatch) => {
         const { data } = await api.fetchPosts();
         // Action must have a type and a payload
         // And dispatch it
-        dispatch({ type: 'FETCH_ALL', payload: data });
+        dispatch({ type: FETCH_ALL, payload: data });
     } catch (error) {
         console.log(error.message);
     }
@@ -17,7 +18,7 @@ api.fetchPosts()
 export const createPost = (post) => async (dispatch) => {
     try {
         const { data } = await api.createPost(post);
-        dispatch({ type: 'CREATE', payload: data });
+        dispatch({ type: CREATE, payload: data });
     } catch (error) {
         console.log(error.message);
 
@@ -27,7 +28,7 @@ export const createPost = (post) => async (dispatch) => {
 export const updatePost = (id, post) => async (dispatch) => {
     try{
         const { data } = await api.updatePost(id, post);
-        dispatch({type: 'UPDATE', payload: data});
+        dispatch({type: UPDATE, payload: data});
     } catch (error) {
         console.log(error.message)
 
@@ -39,7 +40,7 @@ export const deletePost = (id) => async (dispatch) => {
     try{
         await api.deletePost(id);
 
-        dispatch({ type: 'DELETE', payload: id })
+        dispatch({ type: DELETE, payload: id })
     } catch (error) {
         console.log(error);
     }
@@ -48,7 +49,7 @@ export const deletePost = (id) => async (dispatch) => {
 export const likePost = (id) => async (dispatch) => {
     try {
         const { data } = await api.likePost(id);
-        dispatch({type: 'UPDATE', payload: data});
+        dispatch({type: UPDATE, payload: data});
 
     } catch (error) {
         console.log(error)
